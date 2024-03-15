6909b178107753e1eac5189c55b0b6045ae8bc2e
Unenbat <batbaatarunenbat20@gmail.com>
2024-03-15 17:16:02 +0800

    header duuslaa

--- front-app/src/components/app/header/index.tsx
+++ front-app/src/components/app/header/index.tsx
@@ -1,15 +1,42 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
+const headers = [
+  {
+    header: "Cinema",
+    href: "/",
+  },
+  {
+    header: "Coming soon",
+    href: "/comingsoon",
+  },
+  {
+    header: "Event",
+    href: "/event",
+  },
+  {
+    header: "Ticket",
+    href: "/ticket",
+  },
+  {
+    header: "Profile",
+    href: "/profile",
+  },
+];
 const Header = () => {
+  const isActive = usePathname();
   return (
-    <div className="fixed w-[100%] z-10 text-white ml-36 mx-5">
-      <div className="header gap-96 flex py-5  text-red-500 justify-center">
+    <div className="fixed  backdrop-blur-md w-[100%] z-10 text-white pl-36">
+      <div className="header gap-80 flex py-5  text-red-500 justify-center">
         <span className="font-semibold text-lg">CENTRAL CINEMA</span>
         <ul className="flex font-semibold gap-16 text-white">
-          <li className=" text-red-500 ">
+          {/* <li className="">
             <a href="/">Cinema</a>
           </li>
-          <li className=" hover:text-red-400">
+          <li className=" hover:text-red-400 active:text-red-500">
             <a href="/comingsoon">Coming soon</a>
           </li>
           <li className=" hover:text-red-400">
@@ -20,9 +47,21 @@ const Header = () => {
           </li>
           <li className=" hover:text-red-400">
             <a href="#">Profile</a>
-          </li>
+          </li> */}
+          {headers.map((header, i) => (
+            <Link
+              className={`font-bold transition-all duration-75  ${
+                isActive === header.href
+                  ? "text-red-500"
+                  : "hover:text-red-400 "
+              }`}
+              key={i}
+              href={header.href}
+            >
+              {header.header}
+            </Link>
+          ))}
         </ul>
-        <div></div>
       </div>
     </div>
   );
