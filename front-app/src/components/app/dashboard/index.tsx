4ae4abb4bd4b85801c0e852c8292b649ed09f215
Б.Амарболд <b.amarbold0813@yahoo.com>
2024-03-15 16:12:04 +0800

    margin-20 (#22)
    Co-authored-by: Амарболд Батболд <LP230101@G301-CC0192.local>

--- front-app/src/components/app/dashboard/index.tsx
+++ front-app/src/components/app/dashboard/index.tsx
@@ -6,15 +6,14 @@ import EventsBanner from "./eventbanner";
 import CarouselCard from "../carousel";
 import { MovieCard } from "@/components";
 
-
 type Props = {};
 
 export const Dashboard = (props: Props) => {
   return (
-    <div className="">
+    <div className="mx-80">
       <CarouselCard />
       <FilterSection />
-      <MovieCard/>
+      <MovieCard />
       <EventsBanner />
     </div>
   );
