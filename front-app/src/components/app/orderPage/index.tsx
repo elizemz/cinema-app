af9f0d03654ea7ea58e7b20a931d35f8d6385a21
alianu23 <142651654+alianu23@users.noreply.github.com>
2024-03-20 15:17:12 +0800

    backend folder nemsen

--- front-app/src/components/app/orderPage/index.tsx
+++ front-app/src/components/app/orderPage/index.tsx
@@ -1,12 +1,9 @@
-import { Checkout } from "@/components/checkout";
 import React from "react";
 
 type Props = {};
 
 export const OrderPage = (props: Props) => {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <Checkout />
-    </div>
+    <div className="h-screen flex items-center justify-center">OrderPage</div>
   );
 };
