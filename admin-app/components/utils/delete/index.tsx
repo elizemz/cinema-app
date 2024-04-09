a259ce71d19d0622cdf84815fef17be0695cf147
alianu23 <142651654+alianu23@users.noreply.github.com>
2024-04-10 02:37:03 +0800

    delete utils

--- admin-app/components/utils/delete/index.tsx
+++ admin-app/components/utils/delete/index.tsx
@@ -1,5 +1,7 @@
-import { useEvent } from "@/context";
+"use client";
+import { Dialog, Transition } from "@headlessui/react";
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+import { Fragment, useState } from "react";
 
 interface IDelete {
   label?: string;
@@ -9,34 +11,91 @@ interface IDelete {
 }
 
 export const DeleteDialog = ({ label, event, title, handleDelete }: any) => {
-  return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button>Delete</Button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Content maxWidth="450px">
-        <AlertDialog.Title>{title}</AlertDialog.Title>
-        <AlertDialog.Description size="2">{label}</AlertDialog.Description>
+  let [isOpen, setIsOpen] = useState(true);
 
-        <Flex gap="3" mt="4" justify="end">
-          <AlertDialog.Cancel>
-            <Button variant="soft" color="gray">
-              болих
-            </Button>
-          </AlertDialog.Cancel>
-          <AlertDialog.Action>
-            <Button
-              onClick={() => {
-                handleDelete(event._id);
-              }}
-              variant="solid"
-              color="red"
-            >
-              устгах
-            </Button>
-          </AlertDialog.Action>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  const deleteEvent = () => {
+    handleDelete(event._id);
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      <div className=" ">
+        <button
+          type="button"
+          onClick={openModal}
+          className="rounded-md text-sm font-medium text-red-700 hover:bg-red-400 w-full "
+        >
+          Delete
+        </button>
+      </div>
+
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    {title}
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500">{label}</p>
+                  </div>
+
+                  <div className="mt-4 flex gap-3">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-red-200 px-4 py-2 text-sm font-medium text-red-700 hover:bg-red-400 "
+                      onClick={deleteEvent}
+                    >
+                      delete
+                    </button>
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-slate-400 px-4 py-2 text-sm font-medium text-black hover:bg-slate-500 "
+                      onClick={closeModal}
+                    >
+                      cancel
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
   );
 };
