c364d685d496a2321bd71b579d460a02dc776a7d
alianu23 <142651654+alianu23@users.noreply.github.com>
2024-04-10 12:36:58 +0800

    put to post

--- backend-app/src/router/showtime.ts
+++ backend-app/src/router/showtime.ts
@@ -8,7 +8,7 @@ import { authenticate } from "../middleware/auth";
 
 const router = Router();
 
-router.route("/").put(authenticate, updateShowtime).get(getTime);
+router.route("/").post(authenticate, updateShowtime).get(getTime);
 router.route("/new").post(authenticate, createShowtime);
 
 export default router;
